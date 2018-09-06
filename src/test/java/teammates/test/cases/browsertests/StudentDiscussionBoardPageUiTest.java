5a58ef83fbec7d78cc9cde81fe64d2152760e667
pragga <s3657788@student.rmit.edu.au>
2018-09-07 03:52:04 +1000

    Cleaned up StudentDiscussionBoardPageUiTest.java

--- src/test/java/teammates/test/cases/browsertests/StudentDiscussionBoardPageUiTest.java
+++ src/test/java/teammates/test/cases/browsertests/StudentDiscussionBoardPageUiTest.java
@@ -8,6 +8,7 @@ import teammates.common.util.AppUrl;
 import teammates.common.util.Const;
 import teammates.test.driver.BackDoor;
 import teammates.test.pageobjects.StudentDiscussionBoardPage;
+import teammates.test.pageobjects.StudentHomePage;
 
 public class StudentDiscussionBoardPageUiTest extends BaseUiTestCase {
   private StudentDiscussionBoardPage discussionBoardPage;
@@ -17,7 +18,6 @@ public class StudentDiscussionBoardPageUiTest extends BaseUiTestCase {
                 .builder("A1Help", "I need help")
                 .build();
 
-  @Test
   protected void prepareTestData() {
     testData = loadDataBundle("/StudentDiscussionBoardPageUiTest.json");
     removeAndRestoreDataBundle(testData);
@@ -28,13 +28,14 @@ public class StudentDiscussionBoardPageUiTest extends BaseUiTestCase {
     BackDoor.deleteTopic(validTopic.getName()); // delete if it exists
   }
 
-//  @Test
-//  public void allTests() throws Exception {
-//    testAddAction();
-//    testDeleteAction();
-//  }
-
   @Test
+  public void allTests() throws Exception {
+    prepareTestData();
+    testNavLinkToPage();
+    testAddAction();
+    testDeleteAction();
+  }
+
   private void testAddAction() throws Exception {
     discussionBoardPage = getDiscussionBoadPageForStudent("studentWithEmptyProfile");
 
@@ -44,69 +45,23 @@ public class StudentDiscussionBoardPageUiTest extends BaseUiTestCase {
     assertTrue(discussionBoardPage.containsTestTopic());
   }
 
-  @Test
   private void testDeleteAction() throws Exception {
     // Called, assuming the valid topic had been added
     discussionBoardPage.deleteTopic(validTopic.getName());
     // Assert the page doesn't contain the topic
     assertFalse(discussionBoardPage.containsTestTopic());
   }
+  
+  private void testNavLinkToPage() {
+      AppUrl profileUrl = createUrl(Const.ActionURIs.STUDENT_HOME_PAGE)
+                                 .withUserId(testData.accounts.get("studentWithEmptyProfile").googleId);
+      StudentHomePage shp = loginAdminToPage(profileUrl, StudentHomePage.class);
+      discussionBoardPage = shp.loadDiscussionBoardTab();
+    }
 
   private StudentDiscussionBoardPage getDiscussionBoadPageForStudent(String studentId) {
       AppUrl profileUrl = createUrl(Const.ActionURIs.STUDENT_DISCUSSION_BOARD_PAGE)
                                  .withUserId(testData.accounts.get(studentId).googleId);
       return loginAdminToPage(profileUrl, StudentDiscussionBoardPage.class);
   }
-
-
-  // @Override
-  // protected void prepareTestData() throws Exception {
-  //   testData = loadDataBundle("/StudentDiscussionBoardPageUiTest.json");
-  //   removeAndRestoreDataBundle(testData);
-  // }
-  //
-  // @Test
-  // public void allTests() throws Exception {
-  //   testNavLinkToPage();
-  //   testContent();
-  //   testActions();
-  // }
-  //
-  // private void testNavLinkToPage() {
-  //   AppUrl profileUrl = createUrl(Const.ActionURIs.STUDENT_HOME_PAGE)
-  //                              .withUserId(testData.accounts.get("studentWithEmptyProfile").googleId);
-  //   StudentHomePage shp = loginAdminToPage(profileUrl, StudentHomePage.class);
-  //   discussionBoardPage = shp.loadDiscussionBoardTab();
-  // }
-  //
-  // private void testContent() throws Exception {
-  //   // assumes it is run after NavLinks Test
-  //   // (ie already logged in as studentWithEmptyProfile
-  //   //  ______TS("Typical case: empty profile values");
-  //   // Test doesn't run due to java.lang.RuntimeException: org.w3c.dom.DOMException:
-  //   // NAMESPACE_ERR: An attempt is made to create or change an object in a way which
-  //   // is incorrect with regard to namespaces.
-  //   // discussionBoardPage.verifyHtml("/studentDiscussionBoardPageDefault.html");
-  //
-  //   // Test the discussion board test data content
-  //   discussionBoardPage = getDiscussionBoadPageForStudent("studentWithEmptyProfile");
-  //   discussionBoardPage.verifyHtmlPart(
-  //          By.id("topics-table"), "/studentDiscussionBoardTableExistingValues.html");
-  //
-  //   // Test the discussion board form content
-  //   discussionBoardPage = getDiscussionBoadPageForStudent("studentWithEmptyProfile");
-  //   discussionBoardPage.verifyHtmlPart(
-  //          By.className("form"), "/studentDiscussionBoardAddTopicForm.html");
-  // }
-  //
-  // private void testActions() {
-  //   // Test the test data has populated the discussion board
-  //   discussionBoardPage.ensureDiscussionBoardContainsTestData();
-  // }
-  //
-  // private StudentDiscussionBoardPage getDiscussionBoadPageForStudent(String studentId) {
-  //     AppUrl profileUrl = createUrl(Const.ActionURIs.STUDENT_DISCUSSION_BOARD_PAGE)
-  //                                .withUserId(testData.accounts.get(studentId).googleId);
-  //     return loginAdminToPage(profileUrl, StudentDiscussionBoardPage.class);
-  // }
 }
