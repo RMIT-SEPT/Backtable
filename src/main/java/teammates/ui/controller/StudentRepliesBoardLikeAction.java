97f2c2af98bdc2a42f80615106824e8dc85cc450
jeffphan <s3672747@student.rmit.edu.au>
2018-09-30 18:20:17 +1000

    update missing files for up/down vote function

---
+++ src/main/java/teammates/ui/controller/StudentRepliesBoardLikeAction.java
@@ -0,0 +1,55 @@
+package teammates.ui.controller;
+
+import teammates.common.datatransfer.attributes.RepliesAttributes;
+import teammates.common.datatransfer.attributes.TopicAttributes;
+import teammates.common.exception.EntityDoesNotExistException;
+import teammates.common.exception.InvalidParametersException;
+import teammates.common.util.Assumption;
+import teammates.common.util.Const;
+import teammates.common.util.Logger;
+import teammates.ui.pagedata.StudentRepliesBoardEditPageData;
+import teammates.ui.pagedata.StudentRepliesBoardPageData;
+
+import java.util.ArrayList;
+
+/**
+ * Action: showing the profile page for a student in a course.
+ */
+public class StudentRepliesBoardLikeAction extends Action {
+
+    StudentRepliesBoardPageData data;
+    TopicAttributes topic;
+    private static final Logger log = Logger.getLogger();
+
+    @Override
+    protected ActionResult execute() {
+        //getting topic name at the moment, whereas would be better to use topic id
+        String topicId = getRequestParamValue(Const.ParamsNames.TOPIC_ID);
+        Assumption.assertPostParamNotNull(Const.ParamsNames.TOPIC_ID, topicId);
+
+        //receive the id of the reply to be edited and assert not null
+        String replyId = getRequestParamValue(Const.ParamsNames.REPLY_ID);
+        Assumption.assertPostParamNotNull(Const.ParamsNames.REPLY_ID, replyId);
+
+        account.studentProfile = logic.getStudentProfile(account.googleId);
+        data = new StudentRepliesBoardPageData(account, sessionToken);
+        topic = logic.getTopic(topicId);
+        ArrayList<RepliesAttributes> replies = topic.getReplies();
+        RepliesAttributes tmpReply = getReplyWithId(replies, Integer.parseInt(replyId));
+        tmpReply.setLike(tmpReply.getLike()+1);
+        topic.setReplies(replies);
+        try {
+            logic.updateTopic((topic));
+        } catch (InvalidParametersException e) {
+            e.printStackTrace();
+        } catch (EntityDoesNotExistException e) {
+            e.printStackTrace();
+        }
+
+        data.init(topic);
+
+
+        return createShowPageResult(Const.ViewURIs.STUDENT_REPLIES_BOARD_PAGE, data);
+    }
+
+}
